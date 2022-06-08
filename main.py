0b1f6f10330d4366f1d89319e58b200c2ec6b075
Paul Onyebuchi <70877999+ecoderP@users.noreply.github.com>
2022-06-08 15:53:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 class Student:
     # [assignment] Skeleton class. Add your code here
-    def __init__(self):
+    def __init__(self, name, age, tracks, score):
         pass
 
-
+    def __change_name__(self):
+        print("My name is " + Bob.change_name)
+    def __change_age__(self):
+        print("my age is " + Bob.change_age)
+    def __add_track__(self):
+        print("my tracks are " + Bob.tracks)
+    def __get_score__(self):
+        print("My scores are " + Bob.score)
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
@@ -12,3 +19,4 @@ Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
 Bob.get_score()
+
